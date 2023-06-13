f33b313ec6d213b2490dc17c5a3d45ed3f1f836a
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2023-06-13 19:23:56 -0300

    feat: add new command

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -1,12 +1,17 @@
 describe('Página inicial', () => {
   it('Deve renderizar o título principal com o texto correto', () => {
     cy.visit('http://localhost:3000');
+    // Dessa forma utilizamos o comando personalizado
+    cy.getByData('titulo-principal').contains(
+      'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
+    );
     // Podemos utilizar o h1 porem o data-test é mais recomendado.
     // cy.get('h1').contains(
     //   'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
     // );
-    cy.get('[data-test="titulo-principal"]').contains(
-      'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
-    );
+    // Podemos utilizar o get com o data-test
+    // cy.get('[data-test="titulo-principal"]').contains(
+    //   'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
+    // );
   });
 });
