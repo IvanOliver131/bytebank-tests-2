bee828eaeb86120bd9c664cb7afc3fff48d2fa0f
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2023-06-13 19:18:08 -0300

    test: adding tests

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -1,7 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Página inicial', () => {
+  it('Deve renderizar o título principal com o texto correto', () => {
     cy.visit('http://localhost:3000');
-    cy.get('h1').contains(
+    // Podemos utilizar o h1 porem o data-test é mais recomendado.
+    // cy.get('h1').contains(
+    //   'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
+    // );
+    cy.get('[data-test="titulo-principal"]').contains(
       'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
     );
   });
